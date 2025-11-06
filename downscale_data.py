5d61d21d66dcf69b95f6805cdcfded35dc0b8b3d
Majinka10 <camachopinto_@hotmail.com>
2025-11-05 22:38:37 -0500

    fix: updated code to downscale images, now it receives the output shape (h, w)

--- downscale_data.py
+++ downscale_data.py
@@ -14,8 +14,8 @@ def main(args):
 
     for image_path in images:
         image = cv2.imread(os.path.join(args.root_dir, image_path), cv2.IMREAD_UNCHANGED)
-        image = imresize(image, args.scale)
-        
+        image = imresize(image, output_shape=(args.width, args.height))
+
         image_path = f'{os.path.splitext(image_path)[0]}.png'
 
         cv2.imwrite(os.path.join(args.out_dir, image_path), image)
@@ -25,6 +25,7 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Downscale images for SR training.')
     parser.add_argument('--root_dir', required=True, help='Path to the dataset folder.')
     parser.add_argument('--out_dir', required=True, help='Folder target path.')
-    parser.add_argument('--scale', type=float, default=0.1, help='Scale to the downscaled image.')
+    parser.add_argument('--width', type=int, default=64, help='Width of the downscaled image.')
+    parser.add_argument('--height', type=int, default=64, help='Height of the downscaled image.')
     args = parser.parse_args()
     main(args)
